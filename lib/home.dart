804cc49dc503ef3ed99d783a5c2a11f0a38fd582
Shishir707 <mirshr8@gmail.com>
2026-01-29 20:21:44 +0600

    add markers & polyline

--- lib/home.dart
+++ lib/home.dart
@@ -28,13 +28,65 @@ class HomePage extends StatelessWidget {
         mapType: MapType.normal,
         trafficEnabled: true,
         onTap: (LatLng latLng) {
-          //print(latLng);
+          print(latLng);
         },
         onLongPress: (LatLng latLng) {
-          //print("Long Press on $latLng");
+          print("Long Press on $latLng");
         },
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
+        markers: <Marker>{
+          Marker(
+            markerId: MarkerId('my-home'),
+            position: LatLng(23.815583971830566, 90.41056003421545),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueRose,
+            ),
+            visible: true,
+            infoWindow: InfoWindow(
+              title: "My Home",
+              snippet: "Where i live",
+              onTap: () {
+                print("My home info window tapped");
+              },
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('my-office'),
+            position: LatLng(23.816479919246486, 90.41013825684786),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueGreen,
+            ),
+            visible: true,
+            infoWindow: InfoWindow(
+              title: "My Office",
+              snippet: "Where i work",
+              onTap: () {
+                print("My office info window tapped");
+              },
+            ),
+          ),
+        },
+        polylines: <Polyline>{
+          Polyline(
+            polylineId: PolylineId('my-route'),
+            points: [
+              LatLng(23.815583971830566, 90.41056003421545),
+              LatLng(23.816479919246486, 90.41013825684786),
+              LatLng(23.817555293360872, 90.41006147861481),
+              LatLng(23.815986090727016, 90.40961857885122)
+            ],
+            color: Colors.blue,
+            width: 8,
+            visible: true,
+            startCap: Cap.buttCap,
+            endCap: Cap.roundCap,
+            onTap: (){
+              print("Click on Polyline");
+            }
+
+          ),
+        },
       ),
     );
   }
