3b43da23c52ff5c396a15b1c0608d2ad41fbc96a
Shishir707 <mirshr8@gmail.com>
2026-01-29 13:49:12 +0600

    make a normal UI for app

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
+  late final GoogleMapController _mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +14,27 @@ class HomePage extends StatelessWidget {
         ),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(23.815891962604944, 90.4104456016626),
+          zoom: 16,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          controller = _mapController;
+        },
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        mapType: MapType.normal,
+        trafficEnabled: true,
+        onTap: (LatLng latLng) {
+          //print(latLng);
+        },
+        onLongPress: (LatLng latLng) {
+          //print("Long Press on $latLng");
+        },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
       ),
     );
   }
